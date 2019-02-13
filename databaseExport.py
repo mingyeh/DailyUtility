ca92ffa33321e0bc54548c39999523e6d223046a
Ming Yeh <mingyeh1979@hotmail.com>
2019-02-13 15:41:21 +0800

    Add feature of exporting data from multiple db.

--- databaseExport.py
+++ databaseExport.py
@@ -30,6 +30,8 @@ def getDatabaseConfiguration(databaseName):
                 result['password'] = databaseConfigurationNode.get('password')
             if databaseConfigurationNode.get('trustedConnection') is not None:
                 result['trustedConnection'] = databaseConfigurationNode.get('trustedConnection')
+        else:
+            return None
 
     return result
 
@@ -50,91 +52,98 @@ def getDatabaseConnection(configuration):
 
 #Read configuration info
 if not os.path.exists('AppSettings.xml'):
-    print("Cannot find configuration file.")
+    print("ERROR: Cannot find configuration file.")
     exit()
 
 dbConnection = None
 
 #Check argument
+databaseNames = ''
 if len(sys.argv) < 2:
     printUsage()
 else:
     dbNameRe = re.compile(r'^db=(?P<dbName>.+)$')
     dbNameMatch = dbNameRe.match(sys.argv[1])
     if dbNameMatch is not None:
-        dbNameConfig = getDatabaseConfiguration(dbNameMatch.group('dbName'))
-        if dbNameConfig is None:
-            print('Cannot find database configuration in AppSetting.xml')
-        else:
-            dbConnection = getDatabaseConnection(dbNameConfig)
+        databaseNames = dbNameMatch.group('dbName').split(',')
     else:
         printUsage()
 
 #Check SQL script
 sql = pyperclip.paste()
 if len(sql) == 0:
-    print('Please copy SQL script to clipboard before execution.')
+    print('ERROR: Please copy SQL script to clipboard before execution.')
     exit()
 
 print('Acout to execute:\n{sql}\n'.format(sql = sql))
 
-dbCursor = dbConnection.cursor()
-dbCursor.execute(sql)
-
 #Create Excel file
 fileName = 'dataExtract.xlsx'
 wb = openpyxl.workbook.Workbook()
-dataSheet = wb.active
-dataSheet.title = 'Data'
 
 #Read Style configuration
-configurationContent = '\n'.join(open('AppSettings.xml').readlines())
-configurationDocument = BeautifulSoup(configurationContent, features='xml')
-
 headerRowDisplayStyle = {'fontColor':'FFFFFF', 'backgroundColor':'000080'}
 highLightRowDisplayStyle = {'fontColor':'FFFFFF', 'backgroundColor':'FFFF00'}
+
+configurationContent = '\n'.join(open('AppSettings.xml').readlines())
+configurationDocument = BeautifulSoup(configurationContent, features='xml')
 uiConfiguration = configurationDocument.find('uiSettings')
-if not uiConfiguration is None:
+if uiConfiguration is not None:
     headerRowConfiguration = uiConfiguration.find('headerRow')
-    if not headerRowConfiguration is None:
-        if not headerRowConfiguration.get('fontColor') is None:
+    if headerRowConfiguration is not None:
+        if headerRowConfiguration.get('fontColor') is not None:
             headerRowDisplayStyle['fontColor'] = headerRowConfiguration.get('fontColor')
-        if not headerRowConfiguration.get('backgroundColor') is None:
+        if headerRowConfiguration.get('backgroundColor') is not None:
             headerRowDisplayStyle['backgroundColor'] = headerRowConfiguration.get('backgroundColor')
     highLightRowConfiguration = uiConfiguration.find('highLightRow')
-    if not highLightRowConfiguration is None:
-        if not highLightRowConfiguration.get('fontColor') is None:
+    if highLightRowConfiguration is not None:
+        if highLightRowConfiguration.get('fontColor') is not None:
             highLightRowDisplayStyle['fontColor'] = highLightRowConfiguration.get('fontColor')
-        if not highLightRowConfiguration.get('backgroundColor') is None:
+        if highLightRowConfiguration.get('backgroundColor') is not None:
             highLightRowDisplayStyle['backgroundColor'] = highLightRowConfiguration.get('backgroundColor')
 
 headerRowFill = PatternFill(start_color=headerRowDisplayStyle['backgroundColor'],
-                   end_color=headerRowDisplayStyle['backgroundColor'],
-                   fill_type='solid')
+                    end_color=headerRowDisplayStyle['backgroundColor'],
+                    fill_type='solid')
 highLightRowFill = PatternFill(start_color=highLightRowDisplayStyle['backgroundColor'],
-                   end_color=highLightRowDisplayStyle['backgroundColor'],
-                   fill_type='solid')
+                    end_color=highLightRowDisplayStyle['backgroundColor'],
+                    fill_type='solid')
 headerRowFont = Font(color=headerRowDisplayStyle['fontColor'])
 highLightRowFont = Font(color=highLightRowDisplayStyle['fontColor'])
 
-#Write Header row
-columnCount = len(dbCursor.description)
-headerIndex = 1
-for header in dbCursor.description:
-    headerCell = dataSheet.cell(row = 1, column = headerIndex)
-    headerCell.value = header[0]
-    headerCell.fill = headerRowFill
-    headerCell.font = headerRowFont
-    headerIndex += 1
+for databaseName in databaseNames:
+    print('Fetching data from {db}\n'.format(db = databaseName))
+    dbConfig = getDatabaseConfiguration(databaseName)
+    if dbConfig is None:
+        print('ERROR: Cannot find database configuration for {db}\n'.format(db = databaseName))
+        continue
+    
+    dbConnection = getDatabaseConnection(dbConfig)
+    dbCursor = dbConnection.cursor()
+    dbCursor.execute(sql)
 
-rowIndex = 2
-for row in dbCursor.fetchall():
-    for i in range(0, columnCount):
-        dataSheet.cell(row = rowIndex, column = i + 1).value = row[i]
-    rowIndex += 1
+    dataSheet = wb.create_sheet(title = databaseName, index = 0)
+
+    #Write Header row
+    columnCount = len(dbCursor.description)
+    headerIndex = 1
+    for header in dbCursor.description:
+        headerCell = dataSheet.cell(row = 1, column = headerIndex)
+        headerCell.value = header[0]
+        headerCell.fill = headerRowFill
+        headerCell.font = headerRowFont
+        headerIndex += 1
+
+    rowIndex = 2
+    for row in dbCursor.fetchall():
+        for i in range(0, columnCount):
+            dataSheet.cell(row = rowIndex, column = i + 1).value = row[i]
+        rowIndex += 1
+    
+    dbConnection.close()
 
 #Save SQL script
-sqlSheet = wb.create_sheet(title='SQL', index=1)
+sqlSheet = wb.create_sheet(title='SQL', index=len(wb.worksheets)-1)
 sqlSheet['A1'].value = sql
 
 wb.save(fileName)
