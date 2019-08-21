55309f0c382ca9106c5fc1fc549bd580de1f1633
Ming Yeh <mingyeh1979@hotmail.com>
2019-08-21 09:45:35 +0800

    Add header and footer to save sql file.

--- databaseGetDef.py
+++ databaseGetDef.py
@@ -22,6 +22,7 @@ while(True):
 
     objectTypeRow = cursor.fetchone()
 
+    objectType = ''
     if objectTypeRow is None:
         print('Object not found.')
         continue
@@ -34,10 +35,45 @@ while(True):
     cursor.execute('sp_helptext {obj}'.format(obj = objectName))
     definitionRows = cursor.fetchall()
 
+    scriptHeader = """SET XACT_ABORT ON
+
+BEGIN TRANSACTION
+-- Check Database object is exists or not 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[{obj}]') 
+AND type in (N'{objType}'))
+
+-- if object exists then drop object first
+DROP PROCEDURE [dbo].[{obj}]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO\n""".format(obj = objectName, objType = objectType)
+    scriptFooter = '''GO
+
+if @@TRANCOUNT > 0 
+	COMMIT
+GO'''
+    comment = '''
+/**********************************************************************************************************************
+Release      SP Version        Date             Name    			Remarks
+-----------------------------------------------------------------------------------------------------------------------
+XXWXX			1.0				XX-XX-20XX		Ming Ye				PBI XXXX:
+**********************************************************************************************************************/
+'''
+    
     definitionFile = open('{obj}.sql'.format(obj = objectName), 'w')
+    definitionFile.write(scriptHeader)
+    withComment = str(definitionRows[0][0]).startswith('/***')
+    if not withComment:
+        definitionFile.write(comment)
     for row in definitionRows:
         definitionFile.write(row[0].replace('\n', ''))
+    
+    definitionFile.write(scriptFooter)
     definitionFile.close()
+    print('Object saved as {obj}.sql'.format(obj = objectName))
 
 databaseConnection.close()
 print('Database {db} disconnected.'.format(db = databaseName))
