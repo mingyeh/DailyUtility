4cd9718245d63e739b4265b22b7e831614f4e3eb
Ming Ye <ming.ye@volvo.com>
2021-02-24 16:36:46 +0800

    Add perLineCount property to stringConcat script.

--- stringConcat.py
+++ stringConcat.py
@@ -1,19 +1,24 @@
 import pyperclip
 
+perLineCount = 6
+
 def stringConcat(splitBy='\n', joinBy=', ', wrapBy="'"):
     inputString = pyperclip.paste()
     arr = inputString.split(splitBy)
 
     outputString = ''
-
+    counter = 0
     for s in arr:
         if len(s) > 0:
             outputString+=("{wrap}{element}{wrap}{join}"
                            .format(wrap=wrapBy, element=s.strip(), join=joinBy))
-    if len(outputString) > 0:
-        outputString=outputString[:-len(joinBy)]
+        counter += 1
+        if perLineCount > 0:
+            if counter % perLineCount == 0:
+                outputString += '\n'
     pyperclip.copy(outputString)
 
 if __name__ == '__main__':
     stringConcat()
     print('Concated string copied to clipboard')
+    
